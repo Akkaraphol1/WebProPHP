8e30f394b7c609addf5a049b8215af200e2e6890
Akkaraphol1 <6206021630065@fitm.kmutnb.ac.th>
2021-01-20 12:32:17 +0700

    update

--- testform.php
+++ testform.php
@@ -4,8 +4,8 @@
 <body>
 
     <form action="welcome_get.php" method="get">
-        Num X: <input type="text" name="numx"><br>
-        Num Y: <input type="text" name="numy"><br>
+        Num X: <input type="number" name="numx"><br>
+        Num Y: <input type="number" name="numy"><br>
         Operator: <input type="text" name="operator"><br>
         <input type="submit">
     </form>
